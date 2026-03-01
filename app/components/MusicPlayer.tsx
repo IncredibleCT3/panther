bc4b36ef71f248bee0475102ccf7976ec1563a27
Charles <cthorn@emich.edu>
2026-03-01 17:24:28 -0500

    get time function created

--- app/components/MusicPlayer.tsx
+++ app/components/MusicPlayer.tsx
@@ -2,12 +2,33 @@ import React from 'react'
 import SideBar from './SideBar';
 import styles from './MusicPlayer.module.css'
 
+function getTime() {
+    let count=0;
+    const currentDate = new Date();
+    let date = currentDate.toLocaleTimeString();
+    const timeOfDay = date.split(" ")[1];
+
+    for (let i=0; i<=date.length; i++) {
+        if (date[i] === ":") {
+            count++;
+            if (count === 2) {
+                date = date.slice(0, i);
+                break;
+            }
+        }
+    }
+
+    return `${date} ${timeOfDay}`
+}
+
 const MusicPlayer = () => {
   return (
     <div className="main-container flex space-around">
         <SideBar />
         <div className="audio-container flex flex-column space-between">
+            <h2 className='remove-margin google no-bold'>{getTime()}</h2>
             <h1 className={`remove-margin ${styles.titleText} google italic no-bold`}>NOW PLAYING</h1>
+            <h2></h2>
             <div className={`thumbnail-container flex align-end justify-center auto google italic`}>
                 <p className={`${styles.pantherText} muted`}>PANTHER</p>
                 <img src="/pinkpantheress.jpeg" className={`${styles.thumbnailImageSize}`}></img>

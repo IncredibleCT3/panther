69719c2aa8a6b2bd42376c99677cd8ad16fdba58
Charles <cthorn@emich.edu>
2026-03-01 14:49:14 -0500

    audio player css styling

--- app/components/MusicPlayer.tsx
+++ app/components/MusicPlayer.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import SideBar from './SideBar';
+import styles from './MusicPlayer.module.css'
 
 const MusicPlayer = () => {
   return (
     <div className="main-container flex space-around">
         <SideBar />
         <div className="audio-container flex flex-column space-between">
-            <div className="thumbnail-container">
-                
+            <div className={`thumbnail-container flex align-end justify-center auto google italic`}>
+                <p className={styles.pantherText}>PANTHER</p>
+                <img src="/pinkpantheress.jpeg" className={styles.thumbnailImageSize}></img>
             </div>
             <div className="controls-container">
 

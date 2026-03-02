d989397ac39e1b663909cd74a26643142d861816
Charles <cthorn@emich.edu>
2026-03-02 18:32:01 -0500

    updated getDay/getTime logic and styled the positioning

--- app/components/MusicPlayer.tsx
+++ app/components/MusicPlayer.tsx
@@ -7,23 +7,56 @@ function getTime() {
     const currentDate = new Date();
     let date = currentDate.toLocaleTimeString();
     const timeOfDay = date.split(" ")[1];
+    let milliSeconds;
 
     for (let i=0; i<=date.length; i++) {
         if (date[i] === ":") {
             count++;
             if (count === 2) {
+                milliSeconds = date.slice(i+1, i+3);
                 date = date.slice(0, i);
                 break;
             }
         }
     }
-
-    return `${date} ${timeOfDay}`;
+    return [`${date}`, `${milliSeconds}`];
 }
 
 function getDay() {
     const currentDate = new Date();
-    return currentDate.toDateString();
+    const dayOfWeek = currentDate.toLocaleString('en-US', {weekday: 'short',});
+    const monthAbbrev = currentDate.getMonth() + 1;
+    const dayNumber = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    let day = "";
+    let month = "";
+
+    if (dayNumber < 10) {
+        day = `0${dayNumber}`;
+    }
+
+    if (monthAbbrev <= 10) {
+        month = `0${monthAbbrev}`;
+    }
+
+    if (month != "" && day != "") {
+        return `${day}.${month}.${year}`;
+    }
+
+    if (month != "") {
+        return `${dayNumber}.${month}.${year}`;
+    }
+
+    if (day != "") {
+        return `${day}.${monthAbbrev}.${year}`;
+    }
+
+    return `${dayNumber}.${monthAbbrev}.${year}`;
+}
+
+function getDayOfWeek() {
+    const currentDate = new Date();
+    return currentDate.toLocaleString('en-US', {weekday: 'short',}).toUpperCase();
 }
 
 const MusicPlayer = () => {
@@ -31,9 +64,14 @@ const MusicPlayer = () => {
     <div className="main-container flex space-around">
         <SideBar />
         <div className="audio-container flex flex-column space-between">
-            <h2 className='remove-margin google no-bold'>{getTime()}</h2>
-            <h1 className={`remove-margin ${styles.titleText} google italic no-bold`}>NOW PLAYING</h1>
-            <h2 className='remove-margin google no-bold'>{getDay()}</h2>
+            <div className={`flex space-evenly align-end gap ${styles.headerContainer}`}>
+                <h2 className='remove-margin google no-bold center-self'>{getDay()}</h2>
+                <h1 className={`remove-margin ${styles.titleText} google italic no-bold`}>NOW PLAYING</h1>
+                <div className={`flex align-end center-self`}>
+                    <h2 className={`remove-margin google no-bold`}>{`${getDayOfWeek()} ${getTime()[0]}`}</h2>
+                    <p className={`remove-margin google ${styles.small} ${styles.milliMargins}`}>{`${getTime()[1]}`}</p>
+                </div>
+            </div>
             <div className={`thumbnail-container flex align-end justify-center auto google italic`}>
                 <p className={`${styles.pantherText} muted`}>PANTHER</p>
                 <img src="/pinkpantheress.jpeg" className={`${styles.thumbnailImageSize}`}></img>
